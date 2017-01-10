9cfa5e1f734c58b15e7dbbaf77147d192a1b821e
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-10 10:10:06 +0000

    creates a new restaurant and adds it to the index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109170753) do
+ActiveRecord::Schema.define(version: 20170109173417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
