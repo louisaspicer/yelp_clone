439176d58ab8cdd018a55b81efc2ef8841f6052e
James <jamesdix@cwgsy.net>
2017-01-11 18:09:08 +0000

    restaurant review association and link hiding

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,7 +17,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:thoughts, :rating, )
+    params.require(:review).permit(:thoughts, :rating)
   end
 
 end
