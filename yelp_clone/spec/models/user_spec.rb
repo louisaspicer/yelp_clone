4cbe8fef07d9ec2dc1e246a946c14a401116feee
keomony khun <keomony@gmail.com>
2017-01-12 23:33:35 +0000

    user has_many reviewed_restaurants through reviews source restaurant

--- yelp_clone/spec/models/user_spec.rb
+++ yelp_clone/spec/models/user_spec.rb
@@ -1,5 +1,7 @@
-# require 'rails_helper'
-#
-# describe User, type: :model do
-#   it { is_expected.to have_many :reviewed_restaurants, through: :reviews, source: :restaurant }
-# end
+require 'rails_helper'
+
+describe User, type: :model do
+  it { is_expected.to have_many :reviews }
+  it { is_expected.to have_many :restaurants }
+  it { is_expected.to have_many(:reviewed_restaurants).through(:reviews).source(:restaurant) }
+end
